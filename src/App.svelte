e17d2f907a68e0f4a559f500616df6f223cf3a98
Hugo Huart <hugo.huart@heig-vd.ch>
2023-07-04 17:02:14 +0200

    app: initial structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
+  import Match from "./lib/match.svelte";
+  import Playlist from "./lib/playlist.svelte";
+
+  import {artists} from "./stores";
+
+  artists.update((value) => value = ["A", "B"]);
 </script>
 
 <main>
   <h1>Rankify</h1>
+
+  <Match/>
 </main>
 
 <style>
